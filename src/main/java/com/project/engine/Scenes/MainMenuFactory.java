9be0913081cbd0381c072534105668c59fe2234b
Chen Hsuan(Andy) Huang <chenhsuan.huang@mail.utoronto.ca>
2024-11-09 00:14:41 -0500

    Add brief main menu scene and ran in GameMain.java

--- src/main/java/com/project/engine/Scenes/MainMenuFactory.java
+++ src/main/java/com/project/engine/Scenes/MainMenuFactory.java
@@ -2,7 +2,9 @@ package com.project.engine.Scenes;
 
 import com.project.engine.Core.GameObject;
 import com.project.engine.Core.Scene;
+import com.project.engine.Core.Tuple;
 import com.project.engine.Rendering.SpriteRenderer;
+import com.project.engine.UI.GameUILabel;
 
 import java.awt.*;
 
@@ -15,15 +17,28 @@ public class MainMenuFactory {
         SpriteRenderer bgRenderer = new SpriteRenderer("assets/CSC207_asset_bg.png", 800, 800);
         bg.getTransform().setPosition(0, 0);
         bg.addRenderable(bgRenderer);
+        // Cloud
+        GameObject cloud = new GameObject("Cloud");
+        SpriteRenderer cloudRenderer = new SpriteRenderer("assets/CSC207_asset_cloud.png", 800, 800);
+        cloud.getTransform().setPosition(200, 40);
+        cloud.getTransform().setZIndex(1);
+        cloud.getTransform().setScaleX(0.5);
+        cloud.getTransform().setScaleY(0.2);
+        cloud.addRenderable(cloudRenderer);
+        // Game Label
+        GameUILabel title = new GameUILabel("", 50, 100, 600, 300);
+        title.setImage("ui/CSC207_asset_game_title.png");
         // Play Button
-        PlayButton play = new PlayButton(0, 400, 100, 50);
+        PlayButton play = new PlayButton(300, 400, 150, 50);
         play.setBackground(Color.decode("#ffffff"));
         // Leave Button
-        ExitButton leave = new ExitButton(0, 400, 100, 50);
+        ExitButton leave = new ExitButton(300, 500, 150, 50);
         leave.setBackground(Color.decode("#ffffff"));
 
         // Adding everything to the scene
         scene.addSceneObject(bg);
+        scene.addSceneObject(cloud);
+        scene.addUIElement(title);
         scene.addUIElement(play);
         scene.addUIElement(leave);
         return scene;
