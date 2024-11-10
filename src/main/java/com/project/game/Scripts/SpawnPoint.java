4e57cc8a6da4873fcfb5ce9c5e0e5e1ac2946bac
sam198 <jcn3509@gmail.com>
2024-11-10 15:27:38 -0500

    "update death animation"

--- src/main/java/com/project/game/Scripts/SpawnPoint.java
+++ src/main/java/com/project/game/Scripts/SpawnPoint.java
@@ -5,10 +5,17 @@ import com.project.engine.Core.Tuple;
 import com.project.engine.Scripting.IScriptable;
 import com.project.physics.PhysicsBody.RigidBody2D;
 
+
 public class SpawnPoint implements IScriptable {
     private Tuple<Double, Double> spawnPoint;
 
     private GameObject parentGO = null;
+
+    private double respawnDelay = 1.42; // delay in seconds
+
+    private double timeSinceDeath = 0.0;
+
+    private boolean isDead = false;
 
     public SpawnPoint(double x, double y) {
         spawnPoint = new Tuple<>(x, y);
@@ -44,7 +51,22 @@ public class SpawnPoint implements IScriptable {
     public void update(GameObject parent, double deltaTime) {
         parentGO = parent;
         if (parent.getTransform().getPositionY() >= 800) {
-            respawn();
+            Tuple<Double, Double> death_pt = new Tuple<>(parent.getTransform().getPositionX(), parent.getTransform().getPositionY() - 300);
+            parent.getTransform().setPosition(death_pt);
+            parent.getScriptable(RigidBody2D.class).resetY();
+            parent.getScriptable(RigidBody2D.class).resetX();
+            parent.getScriptable(RigidBody2D.class).addForce(0, 1.15f*-300600);
+            parent.getTransform().setScaleX(2.0);
+            isDead = true;
+            timeSinceDeath = 0.0;
+        }
+        if (isDead){
+            timeSinceDeath += deltaTime;
+            if (timeSinceDeath >= respawnDelay) {
+                respawn();
+                isDead = false;
+                parent.getTransform().setScaleX(1.0);
+            }
         }
     }
 
