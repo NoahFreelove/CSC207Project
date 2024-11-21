2211851c4c699331b122f4dd64b42db3dc6b41e6
Kand1Kane <andyhuang941223@gmail.com>
2024-11-21 12:38:55 -0500

    Fix WinOverlay displaying in LevelSelect bug when restart game and back to level select

--- src/main/java/com/project/game/Scenes/LevelSelectionFactory.java
+++ src/main/java/com/project/game/Scenes/LevelSelectionFactory.java
@@ -51,6 +51,7 @@ public class LevelSelectionFactory {
         if (PauseOverlayFactory.isPaused) {
             PauseOverlayFactory.unpauseGame();
         }
+        WinOverlayFactory.removeWinOverlay();
         Scene s = LevelSelectionFactory.createScene();
         Engine.getInstance().unpauseGame();
         w.setActiveScene(s);
